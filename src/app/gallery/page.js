bd2eb4a7f7dabd24204acad1a2a7f0501b477583
humayun khan <humayun.n.k@gmail.com>
2024-07-05 11:49:48 -0400

    added mobile filter and lazy loading

--- src/app/gallery/page.js
+++ src/app/gallery/page.js
@@ -1,6 +1,7 @@
 'use client';
 
 import { useState, useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
 import Header from '../components/Header';
 import HeaderSection from '../components/HeaderSection';
 import AuthorSiteCard from '../components/AuthorSiteCard';
@@ -12,6 +13,10 @@ export default function Gallery() {
   const [filteredWebsites, setFilteredWebsites] = useState([]);
   const [selectedGenre, setSelectedGenre] = useState('All');
   const [genres, setGenres] = useState([]);
+  const [visibleWebsites, setVisibleWebsites] = useState(6); // Number of items to load initially
+  const { ref, inView } = useInView({
+    threshold: 0.1,
+  });
 
   useEffect(() => {
     fetch('/data/websites.json')
@@ -30,7 +35,14 @@ export default function Gallery() {
     } else {
       setFilteredWebsites(websites.filter((site) => site.genre === selectedGenre));
     }
+    setVisibleWebsites(10); // Reset the visible items count when the genre changes
   }, [selectedGenre, websites]);
+
+  useEffect(() => {
+    if (inView) {
+      setVisibleWebsites((prevVisibleWebsites) => prevVisibleWebsites + 6);
+    }
+  }, [inView]);
 
   const handleGenreChange = (genre) => {
     setSelectedGenre(genre);
@@ -43,10 +55,12 @@ export default function Gallery() {
       <div className="container mx-auto p-4">
         <FilterButtons genres={genres} selectedGenre={selectedGenre} onGenreChange={handleGenreChange} />
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 mt-4">
-          {filteredWebsites.map((site) => (
+          {filteredWebsites.slice(0, visibleWebsites).map((site) => (
             <AuthorSiteCard key={site.id} site={site} />
           ))}
         </div>
+        {/* Observer div */}
+        <div ref={ref} className="h-1"></div>
       </div>
       <Footer />
     </>
